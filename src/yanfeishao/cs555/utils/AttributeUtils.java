f218285723e8ca06a663370a7f14f41893c1c39f
Yanfei Shao <shaoyanfei@outlook.com>
2015-10-09 12:00:41 -0400

    Refactoring #40

---
+++ src/yanfeishao/cs555/utils/AttributeUtils.java
@@ -0,0 +1,58 @@
+package yanfeishao.cs555.utils;
+
+import yanfeishao.cs555.constant.ErrorInfo;
+import yanfeishao.cs555.constant.FormatterRegex;
+import yanfeishao.cs555.entities.FamilyEntity;
+import yanfeishao.cs555.entities.PersonEntity;
+import yanfeishao.cs555.enums.ParseEnum;
+
+import java.util.Set;
+
+/**
+ * Created by JackieDreamy on 2015.
+ */
+public class AttributeUtils {
+
+    private AttributeUtils() {
+    }
+
+    /**
+     * Create name factory name utils.
+     *
+     * @return the name utils
+     */
+    public static AttributeUtils createAttributeFactory() {
+        return new AttributeUtils();
+    }
+
+    private String[] splitName(String name) {
+        String[] splitResult = name.split(ParseEnum.PERSON_NAME_SPLIT.toString());
+        return splitResult;
+    }
+
+    private boolean isLegalLastName(PersonEntity child, FamilyEntity familyEntity) {
+        return splitName(child.getName())[1].equals(splitName(familyEntity.getFather().getName())[1]);
+    }
+
+    private boolean us16ParseCondition(FamilyEntity familyEntity, PersonEntity child) {
+        return CommonUtils.isNotNull(child) && !isLegalLastName(child, familyEntity);
+    }
+
+    /**
+     * Parse us 16 error.
+     *
+     * @param result
+     *         the result
+     * @param prefix
+     *         the prefix
+     * @param familyEntity
+     *         the family entity
+     */
+    public void parseUS16Error(Set<String> result, String prefix, FamilyEntity familyEntity) {
+        familyEntity.getChildList().forEach(child -> {
+            if (us16ParseCondition(familyEntity, child)) {
+                result.add(String.format(FormatterRegex.ERROR_PERSON + ErrorInfo.US16, prefix, child.getIdentifier()));
+            }
+        });
+    }
+}
